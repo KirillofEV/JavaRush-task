15b02d4f38a419e03d095bacd7f0e82372248ff9
KirillofEV <KirillofEV@gmail.com>
2016-07-20 17:16:34 +0300

    Add files via upload

---
+++ level13/lesson03/task07/Solution.java
@@ -0,0 +1,27 @@
+package com.javarush.test.level13.lesson02.task07;
+
+/* Параметризованый интерфейс
+В классе StringObject реализуй интерфейс SimpleObject с параметром типа String.
+*/
+
+public class Solution
+{
+    public static void main(String[] args) throws Exception
+    {
+    }
+
+    interface SimpleObject<Object>
+    {
+        SimpleObject<Object> getInstance();
+    }
+
+    public static class StringObject<Object> implements SimpleObject<String>
+    {
+
+        @Override
+        public SimpleObject<String> getInstance()
+        {
+            return new StringObject<Object>();
+        }
+    }
+}
